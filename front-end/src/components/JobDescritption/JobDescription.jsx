7cfcfd7c659db0850d07caf9db8c2033470e6810
Tucker Serwadda <serwadda.tucker14@gmail.com>
2026-01-21 03:36:43 -0500

    created front end for job description (#8)

--- front-end/src/components/JobDescritption/JobDescription.jsx
+++ front-end/src/components/JobDescritption/JobDescription.jsx
@@ -1,6 +1,29 @@
 import { useState } from "react"
 
 const JobDescription=() =>{
+    const [description, setDescription] = useState('');
+    const onClickSubmit = async(ev)=>{
+        ev.preventDefault();
+        if (!description.trim()) return;
+
+        try{
+            await fetch('http://127.0.0.1:8000/job/description',{
+                method:'POST',
+                header:{
+                    'content-Type':'application/json',
+
+                },
+                body: JSON.stringify({
+                    text:description,
+                }),
+
+            })
+            
+            setDescription('')
+        }catch(error){
+            console.error('system failed to submit job description', error)
+        }
+    }
 
 
     <>
@@ -11,6 +34,7 @@ const JobDescription=() =>{
         rows={6}
         value={des_text}
         onChange={(ev)=>setDescription(ev.target.value)}
+        placeholder="paste jobdescription here .... "
         >
 
         </textarea>
